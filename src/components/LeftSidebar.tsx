52f0127dc23e256079b473bb8120dd837b81db51
JoakinGC <joakingcontreras2@gmail.com>
2024-07-26 13:49:24 +0200

    focus left sidebar

--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -1,11 +1,16 @@
-
+'use client';
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { sidebarLinks } from '../../constants'
+import { usePathname, useRouter } from 'next/navigation'
+
+import { cn } from '@/lib/utils'
 
 
 const LeftSideBar = () => {
+  const pathName = usePathname()
+  const router = useRouter
   return (
     <section className="left_sidebar">
         <nav className="flex flex-col gap-6">
@@ -15,9 +20,9 @@ const LeftSideBar = () => {
             </Link>
 
             {sidebarLinks.map(({route,label,imgURL}) =>{
+              const isActive = pathName === route || pathName.startsWith(`${route}/`);
               return (
-                <Link href={route} key={label} className="flex gap-3 items-center py-4
-                max-lg:px-4 justify-center lg:justify-start">
+                <Link href={route} key={label} className={cn("flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start",{"bg-nav-focus border-r-4 border-orange-1": isActive})}>
                   <Image src={imgURL} alt={label} width={24} height={24}/>
                   <p>{label}</p>
                 </Link>
