218fe13aced309a775f8dbd23e6c4683c8ebf196
JoakinGC <joakingcontreras2@gmail.com>
2025-04-09 16:35:01 +0200

    feat: form create podcast page

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -1,13 +1,172 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Label } from "@radix-ui/react-label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { cn } from "@/lib/utils";
+import { Textarea } from "@/components/ui/textarea";
+import GeneratePodcast from "@/components/GeneratePodcast";
+import GenerateThumbnail from "@/components/GenerateThumbnail";
+import { Loader } from "lucide-react";
+import { Id } from "@/convex/_generated/dataModel";
 
-const CreatePodcast = () =>{
+const voiceCategories = ['alloy','shimmer','nova','echo','fable','onyx']
 
-    return(
-        <div>
-            <h1 className='text-20 font-bold text-white-1'>Create Podcast</h1>
+const formSchema = z.object({
+  podcastTitle: z.string().min(2),
+  podcastDescription: z.string().min(2),
+})
+
+const CreatePodcast = ()=>{
+  const [imagePrompt, setImagePrompt] = useState<string>('');
+  const [imageStorageId, setImageStorageId] = useState<Id<"_storage">|null>(null);
+  const [imageUrl, setImageUrl] = useState('')
+
+  const [audioUrl, setAudioUrl] = useState('')
+  const [audioStorageId, setAudioStorageId] = useState<Id<"_storage">|null>(null);
+  const [audioDuration, setAudioDuration] = useState(0)
+
+  const [voiceType, setVoiceType] = useState<string | null>(null);
+  const [voicePrompt, setVoicePrompt] = useState('');
+
+  const [isSubmitting, setSubmitting] = useState<boolean>(false);
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      podcastTitle: "",
+      podcastDescription:"",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+  return (
+    <section className="mt-10 flex flex-col">
+        <h1 className='text-20 font-bold text-white-1'>Create Podcast</h1>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="mt-12 flex w-full flex-col">
+        <div className="flex flex-col gap-[30px] border-b border-black-5 pb-10">
+        <FormField
+          control={form.control}
+          name="podcastTitle"
+          render={({ field }) => (
+            <FormItem className="flex flex-col gap-2.5">
+              <FormLabel className="text-16 font-bold text-white-1">Username</FormLabel>
+              <FormControl>
+                <Input 
+                className="input-class focus-visible:ring-orange-1"
+                placeholder="Podcast" {...field} />
+              </FormControl>
+              <FormMessage 
+              className="text-white-1"/>
+            </FormItem>
+          )}
+        />
+
+        <div className="flex flex-col gap-2.5">
+          <Label className="text-16 font-bold text-white-1">
+            Select AI Voice
+          </Label>
+          <Select onValueChange={(value) => setVoiceType(value)}>
+            <SelectTrigger className={cn('text-16 w-full border-none bg-black-1 text-gray-1')}>
+              <SelectValue 
+              placeholder="Select AI Voice" 
+              className="placeholder:text-gray-1"
+              />
+            </SelectTrigger>
+            <SelectContent
+              className="text-16 border-none bg-black-1 font-bold text-white-1 focus:ring-orange-1"
+            >
+             {voiceCategories.map((category) => (
+              <SelectItem 
+              key={category}
+              value={category}
+              className="capitalize focus:bg-orange-1">
+                {category}
+              </SelectItem>
+             ))}
+            </SelectContent>
+            {voiceType && (
+              <audio 
+                src={`/${voiceType}.mp3`}
+                autoPlay
+                className="hidden"
+              />
+            )}
+          </Select>
+
+          <FormField
+          control={form.control}
+          name="podcastDescription"
+          render={({ field }) => (
+            <FormItem className="flex flex-col gap-2.5">
+              <FormLabel className="text-16 font-bold text-white-1">Description</FormLabel>
+              <FormControl>
+                <Textarea
+                className="input-class focus-visible:ring-orange-1"
+                placeholder="Write a short podcast description" {...field} />
+              </FormControl>
+              <FormMessage 
+              className="text-white-1"/>
+            </FormItem>
+          )}
+        />
+          </div>
         </div>
-    )
+        <div className="flex flex-col pt-10">
+          <GeneratePodcast/>
+
+
+          <GenerateThumbnail />
+
+          <div className="mt-10 w-full">
+            <Button type="submit"
+            className="text-16 w-full bg-orange-1 py-4 font-extrabold text-white-1 transition-all duration-500 hover:bg-black-1">
+              {isSubmitting ? (
+                <>
+                  Submitting
+                  <Loader  size={20} 
+                  className="animate-spin ml-2"/>
+                </>
+              ):(
+                'Submit & Publish Podcast'
+              )}
+            </Button>
+          </div>
+
+        </div>
+      </form>
+    </Form>
+    </section>
+  )
 }
+
 
 
 export default CreatePodcast
