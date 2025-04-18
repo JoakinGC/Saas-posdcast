234d99dcf60323d7758c7a35cdb20e097b04e9f9
JoakinGC <joakingcontreras2@gmail.com>
2025-04-18 22:31:34 +0200

    update: implementation search bar and update discover

--- app/(root)/discover/page.tsx
+++ app/(root)/discover/page.tsx
@@ -1,10 +1,41 @@
+"use client"
+
+import EmptyState from "@/components/EmptyState";
+import LoaderSpinner from "@/components/LoaderSpinner";
+import PodcastCard from "@/components/PodcastCard";
+import SearchBar from "@/components/SearchBar";
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
 import React from "react";
 
-const Discover = () =>{
+const Discover = ({searchParams:{search}} :{searchParams:{search:string}}) => {
+    const podcastsData = useQuery(api.podcasts.getPodcastBySearch,{search:search || ""});
 
     return(
-        <div>
-            <h1 className='text-20 font-bold text-white-1'>Discover </h1>
+        <div className="flex flex-col gap-9">
+            <SearchBar/>
+            <div className="flex flex-col gap-9">
+                <h1 className='text-20 font-bold text-white-1'>Discover </h1>
+                {podcastsData ? (
+                    <>
+                        {podcastsData.length>0 ? (
+                            <div className="podcast_grid">
+                                {podcastsData?.map(({_id,podcastDescription,podcastTitle,imageUrl}) =>{
+                                     return (
+                                        <PodcastCard
+                                            key={_id}
+                                            imgUrl={imageUrl!}
+                                            title={podcastTitle}
+                                            description={podcastDescription}
+                                            podcastId={_id}
+                                        />
+                                    );
+                                })}
+                            </div>
+                        ):<EmptyState title="No resuls Found"/>}
+                    </>
+                ):<LoaderSpinner/>}
+            </div>
         </div>
     )
 }
